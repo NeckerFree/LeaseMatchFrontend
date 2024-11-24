eb20798ee3b3ba942925464a37c25e54d251a3b7
Elio Cortés <elio.cortes3000@gmail.com>
2024-11-24 16:06:20 -0500

    Reset Auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,23 @@ import Navigation from './components/Navigation';
 import PropertiesRegistration from './components/PropertiesRegistration';
 import PropertiesSearch from './components/PropertiesSearch';
 import CustomersRegistration from './components/CustomersRegistration';
-import { useAuthenticator } from "@aws-amplify/ui-react";
+//import { useAuthenticator } from "@aws-amplify/ui-react";
 
-const { user, signOut } = useAuthenticator();
+//const { user, signOut } = useAuthenticator();
 const App: React.FC = () =>
 {
   return (
     <Router>
       <Navigation />
       <div style={{ paddingTop: '3.5rem' }}>
-        <h1>{user?.signInDetails?.loginId}'s todos</h1>
+        {/* <h1>{user?.signInDetails?.loginId}'s todos</h1> */}
         {/* Ensures content does not overlap the navbar */}
         <Routes>
           <Route path="/properties/registration" element={<PropertiesRegistration />} />
           <Route path="/properties/search" element={<PropertiesSearch />} />
           <Route path="/customers/registration" element={<CustomersRegistration />} />
         </Routes>
-        <button onClick={signOut}>Sign out</button>
+        {/* <button onClick={signOut}>Sign out</button> */}
       </div>
     </Router>
   );
