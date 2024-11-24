8f5dc2d5a09ee0d137a090f56ca8c0589a87bba6
Elio Cortés <elio.cortes3000@gmail.com>
2024-11-24 15:51:51 -0500

    Updated App Components structure

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,16 @@ const App: React.FC = () =>
   return (
     <Router>
       <Navigation />
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <div style={{ paddingTop: '3.5rem' }}>
+        <h1>{user?.signInDetails?.loginId}'s todos</h1>
         {/* Ensures content does not overlap the navbar */}
         <Routes>
           <Route path="/properties/registration" element={<PropertiesRegistration />} />
           <Route path="/properties/search" element={<PropertiesSearch />} />
           <Route path="/customers/registration" element={<CustomersRegistration />} />
         </Routes>
+        <button onClick={signOut}>Sign out</button>
       </div>
-      <button onClick={signOut}>Sign out</button>
     </Router>
   );
 };
